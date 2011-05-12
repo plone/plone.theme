0c28a81aca04bfbfcd42c7ef8e15d8092245852d
Hanno Schlichting <hanno@hannosch.eu>
2011-05-12 09:13:37 +0000

    Prepare plone.theme 2.1.
    svn path=/plone.theme/trunk/; revision=49461

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.0.1dev'
+version = '2.1'
 
 long_description = (
     read('README.txt')
