081c7851d15b6e4744d16b33bd04511356f61772
Gil Forcada <gforcada@gnome.org>
2015-04-20 23:44:27 +0200

    Fix more txt -> rst

--- plone/theme/tests/interfaces.py
+++ plone/theme/tests/interfaces.py
@@ -1,6 +1,6 @@
 # A browser layer - you should make one of these for your own skin
 # and register it with a name corresponding to a skin in portal_skins.
-# See tests.zcml and README.txt for more.
+# See tests.zcml and README.rst for more.
 
 from plone.theme.interfaces import IDefaultPloneLayer
 
--- plone/theme/tests/test_doctest.py
+++ plone/theme/tests/test_doctest.py
@@ -8,7 +8,7 @@ from plone.testing import layered
 
 def test_suite():
     return unittest.TestSuite(
-        [layered(doctest.DocFileSuite('README.txt', package='plone.theme',
+        [layered(doctest.DocFileSuite('README.rst', package='plone.theme',
          optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE),
          layer=PLONETHEME_FUNCTIONAL_TESTING)]
     )
