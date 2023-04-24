13dba1cf3934ea1e2bb5e61e65793de99d218e67
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-25 00:08:15 +0200

    feat: pyroma

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     extras_require=dict(
         test=[
             "plone.app.testing",
